9df0ab4480d7dd71054c06e758f164b33b472da4
Mariyam <mariyamakanbi12@gmail.com>
2026-03-03 08:55:07 +0100

    added my first pratical

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string name = "Maryam Akanbi";
+
+int age = 45;
+
+double height = 8.9;
+
+bool isgender = false;
+
+
+
+// String Concatenation
+//Console.WriteLine("My name is " + name + ",I am " + age + "years old. My height is " + height + ". Are you a boy: " + isgender);
+
+//String Interpolation
+Console.WriteLine($"My name is {name}. I am {age} years old. My height is {height}. Are you a boy: {isgender}");
